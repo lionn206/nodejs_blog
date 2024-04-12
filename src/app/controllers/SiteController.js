98351ef4bfed5956589e726a08f4c1716832b331
lionn206 <luund206@gmail.com>
2024-04-12 14:05:20 +0700

    handle&readDB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,16 +1,20 @@
 const Course = require('../models/Course');
-
+const {mutipleMongooseToObject} = require('../../util/mongoose')
 class SiteController {
-    //[GET] /
-    async  index(req, res) {
-        
-        try {
-            const courses = await Course.find({});
-            res.json(courses);
-        } catch (error) {
-            res.status(400).json({ error: 'ERROR!!!' });
-        }
+
+    async  index(req, res, next) {
+
+         //promise
+        Course.find({})
+            .then(courses =>  {
+                res.render('home',{
+                    courses: mutipleMongooseToObject(courses)
+                })
+            })
+            .catch(next);//next:error
     }
+   
+
     
     search(req, res) {
         res.render('search');
---
+++ src/util/mongoose.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mutipleMongooseToObject: function (mongooses){
+        return mongooses.map(mongoose => mongoose.toObject());
+    },
+    mongooseToObject: function (mongooses){
+        return mongooses ? mongoose.toObject(): mongoose;
+    }
+
+};
